1fbb898cc6cadef53e51514c5770cf367d740915
ncardonab <ncardonab@unal.edu.co>
2020-02-15 13:43:36 -0500

    Se creó la carpeta public con los archivos index.html y styles.css, tambien se borró la carpeta assests

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-const express = require('express')
-const path = require('path')
-const fs = require('fs')
-const { ejecutar } = require("child_process")
-const morgan = require('morgan')
-const app = express()
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const { ejecutar } = require("child_process");
+const morgan = require("morgan");
+const app = express();
 
-app.use(morgan('dev'))
-app.use(express.json())
+app.use(morgan("dev"));
+app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req,res) =>{
-  const dir = path.join(__dirname)
-  res.sendFile(path.join(__dirname,'/views/home.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 
-app.listen(8000, (req,res) => {
-  console.log("Servidor en puerto "+ 8000);
+app.listen(8000, (req, res) => {
+  console.log("Servidor en puerto " + 8000);
 });
