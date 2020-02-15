313e46f0ab3b3e30bbaf1b330ddd98f24838473a
ncardonab <ncardonab@unal.edu.co>
2020-02-15 17:02:27 -0500

    Convertido metodo asincrono a sincrono y renderizando la info en formato json

--- index.js
+++ index.js
@@ -1,43 +1,41 @@
-const express = require('express')
-const path = require('path')
-const fs = require('fs')
-const { exec } = require("child_process")
-const morgan = require('morgan')
-const app = express()
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const { exec } = require("child_process");
+const morgan = require("morgan");
+const app = express();
 
-app.use(morgan('dev'))
-app.use(express.json())
+app.use(morgan("dev"));
+app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, "views")));
 
-app.get('/', (req,res) =>{
-  const dir = '/home/sebastianlopez';
-  exec('ls -l',{cwd: dir},(error,stdout,stderror)=>{
-    if(error){
-      console.log('Error:'+ error.menssage);
-      return;
-    }
-    if(stderror){
-      console.log('Stderror:'+ stderror.menssage);
-      return;
-    }
-    let archivosList = stdout.split('\n');
-    archivosList = archivosList.slice(1,archivosList.length-1);
-    let resultadoJ = [];
-    for (var i = 0; i < archivosList.length; i++) {
-      let archivo = archivosList[i].split(' ');
-      resultadoJ.push({
-        permisos : archivo[0].slice(''),
-        tipo : archivo[0].split('')[0],
-        propietario : archivo[2],
-        nombre : archivo[archivo.length-1]
-      });
-    }
-    //console.log(resultadoJ);
-  });
-  res.json(resultadoJ);
+app.get("/", (req, res) => {
+  const directorio = path.join(__dirname, "public");
+
+  // metodo syncrono que espera a que sea completa para seguir
+  const comando = execSync("ls -l", { cwd: directorio }).toString();
+
+  let listaArchivos = comando.split("\n");
+  listaArchivos = listaArchivos.slice(1, listaArchivos.length - 1);
+
+  let infoArchivos = [];
+
+  for (var i = 0; i < listaArchivos.length; i++) {
+    let archivo = listaArchivos[i].split(" ");
+
+    const infoArchivo = {
+      permisos: archivo[0].slice(""),
+      tipo: archivo[0].split("")[0],
+      propietario: archivo[2],
+      nombre: archivo[archivo.length - 1]
+    };
+
+    infoArchivos.push(infoArchivo);
+  }
+  res.json(infoArchivos);
 });
 
-app.listen(8000, (req,res) => {
-  console.log("Servidor en puerto "+ 8000);
+app.listen(8000, (req, res) => {
+  console.log("Servidor en puerto " + 8000);
 });
