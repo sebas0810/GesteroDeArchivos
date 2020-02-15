81c868255b6be66832fc1f093592b5cf830cd3da
Sebastian Lopez <selopezre@unal.edu.co>
2020-02-15 14:55:03 -0500

    Muestra Archivos

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const fs = require('fs')
-const { ejecutar } = require("child_process")
+const { exec } = require("child_process")
 const morgan = require('morgan')
 const app = express()
 
@@ -11,14 +11,14 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, 'views')));
 
 app.get('/', (req,res) =>{
-  const dir = ('~/');
-  ejecutar('ls -l',{cwd: dir},(error,stdout,stderror)=>{
+  const dir = '/home/sebastianlopez';
+  exec('ls -l',{cwd: dir},(error,stdout,stderror)=>{
     if(error){
-      console.log('Error: ${error.menssage}');
+      console.log('Error:'+ error.menssage);
       return;
     }
     if(stderror){
-      console.log('Stderror: ${stderror.menssage}');
+      console.log('Stderror:'+ stderror.menssage);
       return;
     }
     console.log(stdout);
