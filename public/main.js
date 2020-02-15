ce407a612982903873b74e4bd318f7d9ad82ac82
ncardonab <ncardonab@unal.edu.co>
2020-02-15 18:06:32 -0500

    Metodo para traer la info de los archivos desde la API

--- public/main.js
+++ public/main.js
@@ -1,2 +1,14 @@
+// Espera a que el documento se cargue para ejecutar el callback
+window.addEventListener("load", () => {
+  traerArchivos("http://localhost:8000/").then(archivos =>
+    archivos.map(archivo => {
+      console.log(archivo);
+    })
+  );
+});
 
-
+function traerArchivos(enlance) {
+  return fetch(enlance)
+    .then(response => response.json())
+    .then(data => data);
+}
