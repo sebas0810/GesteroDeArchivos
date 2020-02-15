e53a6af76a4e40dfd753ba891c30766b0ef2a4e9
Sebastian Lopez Restrepo <selopezre@unal.edu.co>
2020-02-15 14:12:49 -0500

    Enlistando file/dir

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
-const { ejecutar } = require("child_process");
-const morgan = require("morgan");
-const app = express();
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+const { ejecutar } = require("child_process")
+const morgan = require('morgan')
+const app = express()
 
-app.use(morgan("dev"));
-app.use(express.json());
+app.use(morgan('dev'))
+app.use(express.json())
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'views')));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"));
+app.get('/', (req,res) =>{
+  const dir = ('~/');
+  ejecutar('ls -l',{cwd: dir},(error,stdout,stderror)=>{
+    if(error){
+      console.log('Error: ${error.menssage}');
+      return;
+    }
+    if(stderror){
+      console.log('Stderror: ${stderror.menssage}');
+      return;
+    }
+    console.log(stdout);
+  });
+  res.sendFile(path.join(__dirname,'/views/home.html'));
 });
 
-app.listen(8000, (req, res) => {
-  console.log("Servidor en puerto " + 8000);
+app.listen(8000, (req,res) => {
+  console.log("Servidor en puerto "+ 8000);
 });
