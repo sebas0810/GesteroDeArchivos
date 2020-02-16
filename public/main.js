27fb597337e2e2c1a29aac6329fd96e40433db87
ncardonab <ncardonab@unal.edu.co>
2020-02-16 18:05:59 -0500

    Agregada funcion para ir hacia arriba en la jerarquía de directorios

--- public/main.js
+++ public/main.js
@@ -1,25 +1,55 @@
 // Espera a que el documento se cargue para ejecutar el callback
 window.addEventListener("load", () => {
-  traerArchivos("home/").then(archivos => {
-    // Borra toda la info residual
-    const tabla = document.querySelector(".info-table");
-    tabla.innerHTML = "";
-
-    // Cambiamos el titulo de la carpeta actual
-    const tituloCarpeta = document.querySelector(".folder-name");
-    tituloCarpeta.innerText = "home/";
-
-    // Renderiza tantas filas como archivos hayan
-    archivos.map(archivo => {
-      renderizarInfo(archivo);
-    });
-  });
+  cambioDirectorio("home");
 });
 
 function traerArchivos(directorio) {
   return fetch(`http://localhost:8000/files?directory=${directorio}`)
     .then(response => response.json())
     .then(data => data);
+}
+
+// Evento click cambio de directorios
+document.querySelector(".info-table").addEventListener("click", event => {
+  const clase = event.target.className;
+  const proximaRuta = event.target.textContent;
+  const directorioRaiz = document.querySelector(".folder-name").textContent;
+
+  if (clase === "move-dir") {
+    const ruta = `${directorioRaiz}/${proximaRuta}`;
+
+    cambioDirectorio(ruta);
+  }
+});
+
+// Evento click para ir hacia el directorio padre
+document.querySelector(".go-back").addEventListener("click", () => {
+  const directorioRaiz = document.querySelector(".folder-name").textContent;
+
+  irHaciaAtras(directorioRaiz);
+});
+
+function cambioDirectorio(ruta) {
+  traerArchivos(ruta).then(archivos => {
+    // Borra toda la info residual
+    const tabla = document.querySelector(".info-table");
+    tabla.innerHTML = "";
+
+    // Cambiamos el titulo de la carpeta actual
+    const tituloCarpeta = document.querySelector(".folder-name");
+    tituloCarpeta.innerText = ruta;
+
+    // Renderiza tantas filas como archivos hayan
+    archivos.map(archivo => {
+      renderizarInfo(archivo);
+    });
+  });
+}
+
+function irHaciaAtras(ruta) {
+  const ultimo = ruta.lastIndexOf("/");
+  const rutaAnterior = ruta.substring(0, ultimo);
+  cambioDirectorio(rutaAnterior);
 }
 
 function renderizarInfo(archivo) {
